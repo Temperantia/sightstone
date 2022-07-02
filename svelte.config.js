c2700d6c4465f17fbcc22f432db51ee19a7004a2
Alexandre Du Lorier <adulorier@gmail.com>
2022-07-02 15:31:20 +0200

    change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
-//import node from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-cloudflare";
 import WindiCSS from "vite-plugin-windicss";
 
 const dev = process.env.NODE_ENV == "development";
