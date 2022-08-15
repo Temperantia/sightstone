f3b3c731ca147c4c2156794150fe47b911a45680
Alexandre Du Lorier <adulorier@gmail.com>
2022-08-15 23:34:29 +0200

    no prerender on search

--- src/routes/search.svelte
+++ src/routes/search.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+  export const prerender = false;
+</script>
+
 <script lang="ts">
   import { page } from "$app/stores";
   import FeaturedGame from "$lib/components/FeaturedGame.svelte";
