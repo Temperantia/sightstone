ee8f3d611dc18f16ca55700b1f21a177d5629287
Alexandre Du Lorier <adulorier@gmail.com>
2022-08-17 11:45:34 +0200

    change copyright new name

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -81,7 +81,7 @@
   </div>
 </div>
 <div class="px-10 py-16 text-lg text-white bg-black">
-  © 2022 Streamsniper. Streamsniper isn't endorsed by Riot Games and doesn't
+  © 2022 The Sightstone. The Sightstone isn't endorsed by Riot Games and doesn't
   reflect the views or opinions of Riot Games or anyone officially involved in
   producing or managing League of Legends. League of Legends and Riot Games are
   trademarks or registered trademarks of Riot Games, Inc. League of Legends ©
