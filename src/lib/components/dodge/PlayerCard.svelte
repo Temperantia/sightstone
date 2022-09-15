92c9d57b709a359a0edfa1bef55e19fb86f0b5e8
Alexandre Du Lorier <adulorier@gmail.com>
2022-09-16 00:09:55 +0200

    death note

--- src/lib/components/dodge/PlayerCard.svelte
+++ src/lib/components/dodge/PlayerCard.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
   import type { Player } from "$lib/types";
+  import _ from "lodash";
 
   export let player: Player;
   export let region: string;
+
+  let deathNote = localStorage.getItem("deathNote") ?? "";
 
   const opggRegions: { [region: string]: string } = {
     EUW1: "euw.",
     NA: "na.",
     KR: "",
   };
+
+  $: {
+    if (
+      deathNote.split(",").includes(player.summoner.name) &&
+      !player.tags.includes("DEATH NOTE")
+    ) {
+      player.tags.push("DEATH NOTE");
+      player.tags = player.tags;
+    } else if (!deathNote.split(",").includes(player.summoner.name)) {
+      _.remove(player.tags, (p) => p === "DEATH NOTE");
+      player.tags = player.tags;
+    }
+  }
 
   const tagVariants: { [tag: string]: string } = {
     "Meta Slave": "bg-success text-dark",
@@ -23,6 +39,7 @@
     "Invisibility Abuser": "bg-success text-dark",
     "Lifesteal Abuser": "bg-success text-dark",
     Smurf: "bg-success text-dark",
+    "DEATH NOTE": "bg-dark text-light",
   };
 
   const tagInfo: { [tag: string]: string } = {
@@ -32,13 +49,14 @@
     Tryhard: "Likes hard champs",
     Lucky: "Won the last games",
     Tilted: "Lost the last games",
+    "DEATH NOTE": "You indicated this player is toxic",
   };
 </script>
 
 <div
   class="flex flex-col items-center justify-between p-3 bg-light w-63 h-80 rounded-xl"
 >
-  <div class="flex flex-col items-center w-full space-y-5">
+  <div class="flex flex-col items-center w-full space-y-2">
     <a
       class="text-lg text-darkgray font-inter"
       href="https://{opggRegions[region]}op.gg/summoner/userName={player
@@ -47,6 +65,21 @@
     >
       {player.summoner.name}
     </a>
+    <button
+      class="w-6"
+      on:click={() => {
+        let list = deathNote.split(",").filter((player) => !!player);
+        if (list.includes(player.summoner.name)) {
+          _.remove(list, (p) => p === player.summoner.name);
+        } else {
+          list.push(player.summoner.name);
+        }
+        deathNote = list.join(",");
+        localStorage.setItem("deathNote", list.join(","));
+      }}
+    >
+      <img src="/trash.png" alt="trash" />
+    </button>
     <div class="flex flex-row flex-wrap justify-center space-x-1">
       {#each player.tags as tag}
         <div
@@ -57,7 +90,7 @@
           {tag}
           {#if tagInfo[tag]}
             <span
-              class="absolute z-5 hidden group-hover:flex bg-light -left-5 top-0 -translate-y-full px-2 py-1 rounded-lg text-center text-dark text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700"
+              class="absolute top-0 hidden px-2 py-1 text-sm text-center rounded-lg z-5 group-hover:flex bg-light -left-5 text-dark"
             >
               {tagInfo[tag]}
             </span>
@@ -75,7 +108,7 @@
       >
         <img src="/question.png" class="w-4 h-8" alt="info" />
         <span
-          class="absolute hidden group-hover:flex bg-light -left-15 -top-25 -translate-y-full w-48 px-2 py-1 bg-gray-700 rounded-lg text-center text-dark text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700"
+          class="absolute hidden w-48 px-2 py-1 text-sm text-center rounded-lg group-hover:flex bg-light -left-15 -top-25 text-dark"
         >
           Point based system based on the last 20 games, which analyzes the
           likely performance of the player.
